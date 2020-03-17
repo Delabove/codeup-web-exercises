c12210dbd74531d45c0d5615559e3bffedd9151f
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-16 19:30:28 -0500

    Javascript exercise

---
+++ js/external.js
@@ -0,0 +1,5 @@
+console.log("Hello from external JavaScript!");
+alert('Welcome to my website!');
+
+var userInput = prompt("Just curious...What is your favorite color?");
+alert("Awesome! " + userInput + " is my favorite color too!");
