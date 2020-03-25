f69409a19c85a5a51faa7d43d437ffaa9e4e7ea8
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-25 11:33:14 -0500

    Walkthrough commit 1

--- js/while.js
+++ js/while.js
@@ -1,19 +1,36 @@
 // while loop pt 1
 
-var i= 2;
+var i = 2;
 
-while(i <= 65536){
+while (i <= 65536) {
     console.log(i);
     i *= 2;
 }
 
 // do-while loop pt 2
-
+//my homework code that didnt work:
 var allCones = Math.floor(Math.random() * 50) + 50;
-// var i = 1;
+// // var i = 1;
+//
+// do {
+//    console.log(i = allCones);
+//     i = 50;
+// } while(i <= Math.floor(Math.random() * 5) + 1);
+
+//instructors code:
 
 do {
-   console.log(i = allCones);
-    i = 50;
-} while(i <= Math.floor(Math.random() * 5) + 1);
+    var conesToPurchase = Math.floor(Math.random() * 5) + 1;
+    console.log(allCones + conesToPurchase);
+    if (conesToPurchase <= allCones) {
+        // I can sell the cones
+        console.log(conesToPurchase + ' cones sold..');
+        allCones -= conesToPurchase;
+    } else {
+        // I do not have the cones to sell
+        console.log("Cannot sell you " + conesToPurchase + " cones I only have " + allCones)
+    }
+    console.log(allCones)
+} while (allCones > 0);
 
+console.log("Yay! I sold them all!!");
