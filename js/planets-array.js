29d74dcc0b1024be21d7b72f6ce5efef60415126
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-26 10:50:12 -0500

    Commit 2 of manipulating arrays exercise

--- js/planets-array.js
+++ js/planets-array.js
@@ -39,14 +39,34 @@
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
 
+    planets.shift("The Sun");
+
+    console.log("After shift:");
+    console.log(planets);
+
     console.log('Removing "Pluto" from the end of the planets array.');
+    console.log(planets);
+
+    planets.pop("Pluto");
+    console.log("After pop:")
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
 
+    console.log(planets.indexOf("Earth"));
+
     console.log("Reversing the order of the planets array.");
+    console.log(planets);
+
+    planets.reverse();
+    console.log("After reverse:")
     console.log(planets);
 
     console.log("Sorting the planets array.");
     console.log(planets);
+
+    planets.sort();
+    console.log("After sort:");
+    console.log(planets);
+
 })();
