446d755ee221903e3438ca7b04ed511241bc2461
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-26 10:41:27 -0500

    Commit 1 of manipulating arrays exercise

---
+++ js/planets-array.js
@@ -0,0 +1,52 @@
+(function(){
+    "use strict";
+    // console.log(Hello);
+
+
+    var planets = [
+        'Mercury',
+        'Venus',
+        'Earth',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+    ];
+
+    /**
+     * TODO:
+     * Read each console log below, and write some javascript code to perform
+     * the step that it describes
+     */
+
+    console.log('Adding "The Sun" to the beginning of the planets array.');
+    console.log(planets);
+
+   planets.unshift("The Sun");
+   console.log("After unshift:");
+   console.log(planets);
+
+
+    console.log('Adding "Pluto" to the end of the planets array.');
+    console.log(planets);
+
+    planets.push("Pluto");
+
+    console.log("After the push:");
+    console.log(planets);
+
+    console.log('Removing "The Sun" from the beginning of the planets array.');
+    console.log(planets);
+
+    console.log('Removing "Pluto" from the end of the planets array.');
+    console.log(planets);
+
+    console.log('Finding and logging the index of "Earth" in the planets array.');
+
+    console.log("Reversing the order of the planets array.");
+    console.log(planets);
+
+    console.log("Sorting the planets array.");
+    console.log(planets);
+})();
