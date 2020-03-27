5aa75d78093f6e30c99139af2dfed394caab7c97
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-27 15:34:14 -0500

    commit 4 of codecademy conditionals

--- js/codecademy.js
+++ js/codecademy.js
@@ -14,3 +14,47 @@ if(sale) {
 
 // Below the sale variable declaration, but before the if statement, reassign sale to false. Run your code and observe what happens, we’ll be changing this behavior in the next exercise.
 let sale = false;
+
+// Add an else statement to the existing if statement. Inside the code block of the else statement, console.log() the string 'Time to wait for a sale.'
+
+let sale = true;
+
+sale = false;
+
+if(sale) {
+    console.log('Time to buy!');
+} else {
+    console.log("Time to wait for a sale.")
+}
+
+// Using let, create a variable named hungerLevel and set it equal to 7.
+
+let hungerLevel = 7;
+
+// Write an if...else statement using a comparison operator. The condition should check if hungerLevel is greater
+// than 7. If so, the conditional statement should log, 'Time to eat!'. Otherwise, it should log 'We can eat later!'.
+//     After you press run, play around with the condition by tweaking the comparison of hungerLevel by using different
+// operators such as <=,>=,>, and <.
+
+if(hungerLevel > 7) {
+    console.log("Time to eat!");
+} else {
+    console.log("We can eat later!");
+}
+
+// Let’s create an if...else statement that
+// checks if mood is 'sleepy' and tirednessLevel
+// is greater than 8.
+//
+// If both conditions evaluate to true, then
+// console.log() the string 'time to sleep'. Otherwise, we
+// should console.log() 'not bed time yet'.
+
+let mood = 'sleepy';
+let tirednessLevel = 6;
+
+if(mood === sleepy  && tirednessLevel > 8) {
+    console.log("time to sleep")
+} else {
+    console.log("not bed time yet");
+}
