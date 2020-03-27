726ac15a6f5f5b022f0361d177fc4de24fbfb0a6
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-27 16:24:04 -0500

    commit 6 codecademy

--- js/codecademy.js
+++ js/codecademy.js
@@ -87,3 +87,19 @@ if (favoritePhrase) {
 }
 
 
+// Let’s use short-circuit evaluation to assign a value to
+// writingUtensil.Do not edit tool yet, we’ll return to tool
+// in the next step. Assign to writingUtensil the value of
+// tool and if tool is falsy, assign a default value of 'pen'.
+
+let writingUtensil = tool || 'pen';
+
+//What if we reassign the value of tool to 'marker'. Let’s
+// see what happens to the value of writingUtensil.
+
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
