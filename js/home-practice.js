c39aa9235bcda3ba77aec604ccce8e407d5c446d
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-26 20:54:35 -0500

    commit 1 of home practice

---
+++ js/home-practice.js
@@ -0,0 +1,20 @@
+function sayHello(){
+}
+console.log(sayHello);
+
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function addition(num) {
+    return num + 1;
+}
+
+// There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+function remainderOfTwoNum(x,y) {
+    return x % y;
+}
+
+// Write a function that takes an integer minutes and converts it to seconds.
+
+function minToSec(minutes) {
+    return minutes * 60;
+}
