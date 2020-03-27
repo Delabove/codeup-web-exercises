d3d33ffd67c9d9918fd6938bad490627cd15c067
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-27 14:27:10 -0500

    commit 2 free code camp practice

--- js/freecodecamp.js
+++ js/freecodecamp.js
@@ -13,3 +13,23 @@ var ourName;
 
 var a = 7;
 var b = a;
+
+
+
+// Define a variable a with var and initialize it to a value of 9.
+var a = 9;
+
+
+
+// Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+// Only change code below this line
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
