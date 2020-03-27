d442885738811b31bafd1fd9b3271079fc0ed30c
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-27 14:17:12 -0500

    freecodecamp commit 1

---
+++ js/freecodecamp.js
@@ -0,0 +1,15 @@
+
+console.log("Hello, World!");
+
+// Use the var keyword to create a variable called myName.
+
+var ourName;
+
+// Assign the value 7 to variable a.
+//
+//     Assign the contents of a to variable b.
+
+
+
+var a = 7;
+var b = a;
