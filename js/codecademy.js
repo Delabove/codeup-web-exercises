dfd4fe6dfc26bbc56fdc5c2d9446be96cca52e34
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-28 18:41:40 -0500

    Commit 4 of practice exercises

--- js/codecademy.js
+++ js/codecademy.js
@@ -226,3 +226,24 @@ function logCitySkyline()  {
 }
 
 console.log(logCitySkyline());
+
+// write three global variables:
+//
+//     Name the first variable satellite and set it equal to 'The Moon'.
+//     Name the second variable galaxy and set it equal to 'The Milky Way'.
+//     Name the third variable stars and set it equal to 'North Star'.
+// Below the variables created in the previous step, write a function named callMyNightSky. Inside the function, include a return statement like this:
+//return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+// Beneath the callMyNightSky() function, use console.log() to log the value of callMyNightSky() to the console.
+
+
+
+
+var satellite = 'The Moon';
+var galaxy = 'The Milky Way';
+var stars = 'North Star';
+
+function callMyNightSky() {
+    return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+console.log(callMyNightSky());
