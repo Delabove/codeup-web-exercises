df5a4c05c14567d7de90e6dd7afaf94d53b3ec1f
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-28 18:07:53 -0500

    commit 2 of codecademy exercises

--- js/codecademy.js
+++ js/codecademy.js
@@ -188,3 +188,26 @@ function costOfMonitors(rows,columns) {
 }
 const totalCost = costOfMonitors(5,4);
 console.log(totalCost);
+
+
+// Let’s say we have a plant that we need to water once a week on Wednesdays. We could define a function expression to help us check the day of the week and the plant needs to be watered:
+//
+//     Create a variable named plantNeedsWater using the const variable keyword.
+//     Assign an anonymous function that takes in a parameter of day to plantNeedsWater.
+//      In the function body add an if conditional that checks day === 'Wednesday'.
+//     If the conditional is truthy, inside the if code block, use the return keyword to return true.
+//      Add an else statement after the if statement.
+//        Inside the else statement use the return keyword to return false.
+// Call the plantNeedsWater() and pass in 'Tuesday' as an argument.
+
+
+
+const plantNeedsWater = function(day) {
+    if (day === 'Wednesday') {
+        return true;
+    } else {
+    return false;
+    }
+};
+console.log(plantNeedsWater('Tuesday'));
+
