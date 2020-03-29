5924d38fd60348b09596ae211635d61747321947
DeLayne LaBove <delayne.labove@gmail.com>
2020-03-28 20:32:56 -0500

    commit 10 of practice exercises

--- js/codecademy.js
+++ js/codecademy.js
@@ -325,3 +325,12 @@ function getLastItem(arr) {
     return arr[arr.length -1];
 }
 
+// Create a function that takes two strings as arguments and return either true or false depending on whether the total number of characters in the first string is equal to the total number of characters in the second string.
+function comp(str1, str2) {
+    return str1.length === str2.length;
+}
+
+// Given two integers, a and b, return true if a can be divided evenly by b. Return false otherwise.
+function dividesEvenly(a, b) {
+    return a % b === 0;
+}
