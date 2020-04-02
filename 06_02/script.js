f319d98babe2d58be5bf5f032b4858b973375fd4
DeLayne LaBove <delayne.labove@gmail.com>
2020-04-02 07:41:19 -0500

    commit 2 of analog clock

--- 06_02/script.js
+++ 06_02/script.js
@@ -1,3 +1,15 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+// will let the degrees we want to turn the arms
+
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+
+// apply above numbers as degrees as inline styles for transform on each object. apply the styles on above variables.
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + minPosition + "deg)";
