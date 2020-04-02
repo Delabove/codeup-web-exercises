3343a4020d591d3da801dfc2ac8c1898db7ed668
DeLayne LaBove <delayne.labove@gmail.com>
2020-04-02 16:29:03 -0500

    commit 3 of exercise

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -8,8 +8,24 @@ var map = new mapboxgl.Map({
     center: [-96.785530, 32.782990],
 });
 
+var markerOptions = {
+    color: "pink",
+    draggable: true
+};
+var marker = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-96.785530, 32.782990])
+    .addTo(map);
+
 geocode("2615 Commerce St., Dallas, TX 75226", mapBoxToken).then(function(result) {
     console.log(result);
     map.setCenter[-96.785530, 32.782990];
     map.setZoom(18);
-})
+});
+
+var popup = new mapboxgl.Popup()
+    .setHTML ("<h1>Love Twisted Root!</h1>")
+    .addTo(map);
+
+marker.setPopup(popup);
+var popupFavRest = new mapboxgl.Popup()
+    .addTo(map);
