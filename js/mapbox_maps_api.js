ac4feb0d889694d5fa4a355de7cd4b39ea066e81
DeLayne LaBove <delayne.labove@gmail.com>
2020-04-02 15:46:50 -0500

    centered and coordinate

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -3,11 +3,13 @@ console.log("token: " + mapBoxToken);
 mapboxgl.accessToken = mapBoxToken;
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 5,
+    center: [-96.785530, 32.782990],
 });
 
 geocode("2615 Commerce St., Dallas, TX 75226", mapBoxToken).then(function(result) {
     console.log(result);
-    map.setCenter(result);
-    map.setZoom(20);
+    map.setCenter[-96.785530, 32.782990];
+    map.setZoom(18);
 })
