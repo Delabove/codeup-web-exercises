108c56cd5bbdf212b80e713ae40e802d0c097d91
DeLayne LaBove <delayne.labove@gmail.com>
2020-04-03 11:48:50 -0500

    Commit 2 of walkthrough

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -11,39 +11,22 @@ var map = new mapboxgl.Map({
 
 map.setZoom(10);
 
-geocode("2615 Commerce St., Dallas, TX 75226", mapBoxToken).then (function(coordinates) {
-    var marker = new mapboxgl.Marker()
-        .setLngLat([-96.785530, 32.782990])
-        .addTo(map);
-
-    var popup = new mapboxgl.Popup()
-        .setHTML ("<h1>Love Twisted Root!</h1>")
-
-    marker.setPopup(popup);
-
-});
-
-
-
-var favRest = ["Cane Rosso", "Nori Handroll Bar", "Nola Brasserie"];
-console.log(favRest.length);
-
-var favRest = [
+var restaurantArray = [
     {
-        rest1: "Cane Rosso",
+        popupHTML: "Cane Rosso",
         type: "Pizza",
         coordinates: [-96.764700, 32.991530],
         address: "2612 Commerce St. Dallas, Tx 75226"
     },
     {
-        rest2: "Nori Handroll Bar",
+        popupHTML: "Nori Handroll Bar",
         type: "Sushi",
         coordinates: [-96.764700, 32.991530],
         address: "2814 Elm St. Dallas, Tx 75226"
     },
 
     {
-        rest2: "Nola Brasserie",
+        popupHTML: "Nola Brasserie",
         type: "Louisiana Cuisine",
         coordinates: [-96.764700, 32.991530],
         address: "1201 Main St, Ste S100 Dallas, Texas 75202"
@@ -51,7 +34,19 @@ var favRest = [
 ];
 
 
-favRest.forEach(function (restaurant) {
+    restaurantArray.forEach(function (restaurant) {
     console.log(restaurant);
 
+    geocode(restaurantArray.address, mapBoxToken).then (function(coordinates) {
+    var marker = new mapboxgl.Marker()
+        .setLngLat(coordinates)
+        .addTo(map);
+
+    var popup = new mapboxgl.Popup()
+        .setHTML (restaurant.popupHTML)
+
+    marker.setPopup(popup)
 });
+});
+
+
