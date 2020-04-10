e6e5dc64935895586907aacabf45a9a20fc28de3
DeLayne LaBove <delayne.labove@gmail.com>
2020-04-10 07:51:54 -0500

    identifying functions

--- js/study-guide.js
+++ js/study-guide.js
@@ -34,3 +34,23 @@ function filterArray(age) {
 
 
 console.log(filterArray());
+
+function evenArray() {
+    let arr = [1, 2, 3, 4, 5, 6];
+    let even = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) even.push(arr[i]);
+    }
+    return even;
+}
+
+console.log(evenArray());
+
+// OR
+
+let arr = [1, 2, 3, 4, 5, 6];
+    let even = arr.filter(val => {
+        return val % 2 === 0;
+    });
+
+console.log(even);
